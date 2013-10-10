cd87e145d58020c4314260f7f17bcfa1d739a2e2
Gaurav Ghimire <gaurav@subisu.net.np>
2013-10-10 11:00:31 +0545

    changed random generation from openssl to /dev/random/

--- remotebashrc.sh
+++ remotebashrc.sh
@@ -5,6 +5,10 @@
 # Modified by: Gaurav Ghimire 
 # For Updates, visit: http://www.justynshull.com/projects/remotebashrc.html
 # Last Updated: 12/01/2009
+# put this in your ~/.ssh/config file this is needed for the below script to work
+# Host *
+# ControlMaster auto
+# ControlPath ~/.ssh/master-%r@%h:%p
 
 # Usage: remotebashrc user port host
 # ex: ./remotebashrc.sh root 22 10.0.0.2
@@ -14,9 +18,9 @@
 
 ##TODO
 # get this script converted from svn to git and working
-# random_file doesn't always work. $RANDOM returns nothing some times.
 # check to see if it's a freebsd box before trying to ssh in.. /bin/bash doesn't seem to be installed by default
 #[ $# = 0 ] && { sed -n -e '/^# Usage:/,/^$/ s/^# \?//p' < $0; exit; }
+
 #debug=1 # debug on, to disable comment it out
 ruser=$1
 rport=$2
@@ -27,7 +31,8 @@ base_bashrc=~/.ssh/bashrc-remote
 bashrc_remote=/tmp/bashrc_remote.$rhost
 cp $base_bashrc $bashrc_remote
 # random_file, used openssl to generate more random characters
-random_file=/tmp/rshrc-`date +%d%m%g%H%M%S`.`/usr/bin/openssl rand -base64 10 | awk -F= '{print $1}'`
+# LC_CTYPE for tr in OSX 
+random_file=/tmp/rshrc-`date +%d%m%g%H%M%S`.`cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
 # Appended random file details to the bashrc_remote to be uploaded to remote server
 # The attached "rm $file" would remove the bashrc_remote file just after bash is spawned
 # This is done so, because in case of network disconnects and when you don't get to logout
@@ -35,9 +40,7 @@ random_file=/tmp/rshrc-`date +%d%m%g%H%M%S`.`/usr/bin/openssl rand -base64 10 | 
 echo "\nrandom_file=$random_file\nrm -f $random_file" >> $bashrc_remote
 f_debug() { [ $debug ] && echo "$1" && echo "hello"; }
 f_debug "initiating master connection.."
-#SSH in on server, my SSH ~/.ssh/config already has below in it, you might want to add in yours
-# ControlMaster auto
-# ControlPath ~/.ssh/ssh_master-%r_%h_%p
+#SSH in on server
 ssh -Nfn $ruser@$rhost -p $rport
 f_debug "uploading $random_file to remote server..."
 #scp using the same stream, mode changed to be quiet
